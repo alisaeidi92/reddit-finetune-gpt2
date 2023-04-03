7d8654aed253d7b2f611374d70a150c56abbd6b4
alisaeidi92 <alisaeidi92@yahoo.com>
2023-04-03 17:07:06 +0000

    comments and formatting

--- GPT2-baseline.py
+++ GPT2-baseline.py
@@ -1,54 +1,49 @@
+# Import necessary libraries and modules
+import torch
+from transformers import (
+    GPT2Tokenizer,
+    DataCollatorForLanguageModeling,
+    AutoModelForCausalLM,
+    TrainingArguments,
+    Trainer,
+)
+import datasets
+from sklearn.model_selection import train_test_split
+import numpy as np
+import evaluate
 
-# Define the device to use (GPU or CPU)
-device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+# Clear any cached memory on the GPU
+torch.cuda.empty_cache()
+
+# Determine the device to use (GPU or CPU) based on availability
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 # Load the Reddit dataset and split it into train and test sets
-#dataset = datasets.load_dataset('reddit')['train']
+dataset = datasets.load_dataset("reddit")["train"].train_test_split(test_size=0.1)
 
-dataset =  datasets.load_dataset('reddit')['train'].train_test_split(test_size=0.1)
+# Subset the train and test datasets for faster experimentation
+train_dataset = dataset["train"][:100000]
+test_dataset = dataset["test"][:10000]
 
-train_dataset = dataset['train'][:100000] #keeping 1 million reviews for training
-test_dataset = dataset['test'][:10000] # keeping 150k reviews for testing
+# List of columns to remove from the dataset
+rem_list = ["author", "body", "id", "normalizedBody", "subreddit", "subreddit_id"]
 
-#train_dataset = datasets.load_dataset('reddit')['train'].shuffle(seed=42)
-#test_dataset = datasets.load_dataset('reddit')['train'].shuffle(seed=42)
-
-
-rem_list = ['author', 'body', 'id', 'normalizedBody', 'subreddit', 'subreddit_id']
-
+# Remove unnecessary columns from the train and test datasets
 print("\n Removing extra columns.")
 [train_dataset.pop(key) for key in rem_list]
 [test_dataset.pop(key) for key in rem_list]
 print("\nDONE!\n")
 
-###print("\n\n\nDataset:\n", dataset)
-
-##dataset = dataset.remove_columns(['author', 'body', 'id', 'normalizedBody', 'subreddit', 'subreddit_id'])
-
-#print("\n\n keys: ", list(dataset.keys()))
-
-##train_dataset, test_dataset = train_test_split(dataset, test_size=0.2)
-
-##print("\n\n\nTRAIN DATA KEYS:", train_dataset.keys())
-##print("\n\n\nTEST DATA KEYS:", test_dataset.keys())
-##print("\n\n\nTRAIN DATA CONTENT: \n", train_dataset['content'][0])
-##print("\n\n\nTEST DATA CONTENT: \n", test_dataset['content'][0])
-##print("\n\n\nTRAIN DATA SUMM: \n", train_dataset['summary'][0])
-##print("\n\n\nTEST DATA SUMM: \n", test_dataset['summary'][0]) 
-
-##exit()
-
 # Define the GPT-2 model and tokenizer
-model_name = 'gpt2'
+model_name = "gpt2"
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
-#tokenizer = AutoTokenizer.from_pretrained(model_name)
 tokenizer.pad_token = tokenizer.eos_token
 model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
-#model = AutoModelForSeq2SeqLM.from_pretrained(model_name).to(device)
-# Define the training arguments
+
+# Set the training arguments
 training_args = TrainingArguments(
-    output_dir='./results',
-    evaluation_strategy='steps',
+    output_dir="./results",
+    evaluation_strategy="steps",
     eval_steps=500,
     save_steps=500,
     save_total_limit=3,
@@ -58,117 +53,93 @@ training_args = TrainingArguments(
     learning_rate=2e-5,
     gradient_checkpointing=True,
     fp16=True,
-    logging_dir='./logs',
-    #auto_find_batch_size=True,
+    logging_dir="./logs",
     logging_steps=500,
     logging_first_step=True,
     dataloader_num_workers=2,
-    #predict_with_generate=True,
-    #use_cache=False
 )
 
-# Define the function to preprocess the data
+
+# Define a function to preprocess the data
 def preprocess_function(examples):
-    ##print("\n\nLEN EXAMPLE:", len(examples))
-    ##print("\n\nTYPE EXAMPLE: ", type(examples))
-    ##print("\n\n[0] EXAMPLE:", list(examples.keys()))
-    ##print("\n\nEXAMPLE: ", examples)
     inputs = []
     targets = []
-    for i in range(len(examples['content'])):
-        content = str(examples['content'][i])
-        target = str(examples['summary'][i])
+    for i in range(len(examples["content"])):
+        content = str(examples["content"][i])
+        target = str(examples["summary"][i])
         if content.strip() and target.strip():
             inputs.append(content + " TL;DR " + target)
             targets.append(target)
 
-            ##print(f"Content: '{content}'")
-            ##print(f"Target: '{target}'")
-            ##print("\n"*10)
-    #
-    model_inputs = tokenizer(inputs, max_length=512, return_tensors="pt", padding='max_length', return_overflowing_tokens=False, truncation=True)
+    # Tokenize input sequences
+    model_inputs = tokenizer(
+        inputs,
+        max_length=512,
+        return_tensors="pt",
+        padding="max_length",
+        return_overflowing_tokens=False,
+        truncation=True,
+    )
     with tokenizer.as_target_tokenizer():
-       labels = tokenizer(targets, max_length=32, return_tensors="pt", padding='max_length', return_overflowing_tokens=False, truncation=True)
-    
-    model_inputs['labels'] = labels['input_ids']
-    
+        labels = tokenizer(
+            targets,
+            max_length=32,
+            return_tensors="pt",
+            padding="max_length",
+            return_overflowing_tokens=False,
+            truncation=True,
+        )
+
+    model_inputs["labels"] = labels["input_ids"]
+
     return model_inputs
 
-# convert train and test dictionaries to Dataset object.
+
+# Convert train and test dictionaries to Dataset objects
 train_dataset = datasets.Dataset.from_dict(train_dataset)
 test_dataset = datasets.Dataset.from_dict(test_dataset)
 
 # Preprocess the train and test datasets
-train_dataset = train_dataset.map(preprocess_function, batched=True) #remove_columns=["content", "summary"])
-test_dataset = test_dataset.map(preprocess_function, batched=True) # remove_columns=["content", "summary"])
+train_dataset = train_dataset.map(preprocess_function, batched=True)
+test_dataset = test_dataset.map(preprocess_function, batched=True)
 
-#print("\n\nlen train dataset: ", len(train_dataset))
-#print("\n\nlen test dataset: ", len(test_dataset))
-#print("\n\ntype train dataset: ", type(train_dataset))
-#print("\n\ntype test dataset: ", type(test_dataset))
-
-
-# Define the ROUGE metric
-##rouge = datasets.load_metric('rouge')
-
-# Define the compute metrics function
-"""
-def compute_metrics(pred):
-    labels_ids = pred.label_ids
-    pred_ids = pred.predictions
-    pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
-    labels_ids[labels_ids == -100] = tokenizer.pad_token_id
-    label_str = tokenizer.batch_decode(labels_ids, skip_special_tokens=True)
-    rouge_output = rouge.compute(predictions=pred_str, references=label_str, rouge_types=['rouge2'])['rouge2'].mid.fmeasure
-    return {'rouge2': rouge_output}
-"""
-  
-
+# Load the ROUGE metric
 rouge_score = evaluate.load("rouge")
 
+
+# Define a function to compute the metrics (ROUGE scores)
 def compute_metrics(eval_pred):
     predictions = eval_pred.predictions
     labels = eval_pred.label_ids
-    #predictions, labels = eval_pred
-    # Decode generated summaries into text
-    #print("\n\n PREDICTION: \n", predictions[0])                                                                                                                                                                                            
-    #print("\n\n LEN:n", len(predictions))
-    #print("\n\n type:", type(predictions))
-    #print("\n\n shape:", predictions.shape)
-#   pred_ids = predictions['input_ids']
-#   labels_ids = labels['input_ids']
-    #predictions = predictions.reshape(-1, predictions.shape[-1])  # Reshape to 2D array
-    #predictions = predictions.tolist()
-    #labels = labels.tolist()
-
-    #predictions = predictions.reshape(-1, )
-    #labels = labels.reshape(-1, )
-    predictions = predictions.argmax(axis=-1)     
-    #predictions = predictions.transpose(2,0,1).reshape(-1,predictions.shape[1])
-    #labels = labels.transpose(2,0,1).reshape(-1,labels.shape[1])
+    predictions = predictions.argmax(axis=-1)
+    # Decode the predicted summaries into text
     decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
-
-    #print("\n\n\n\n decoded: \n", decoded_preds)
 
     # Replace -100 in the labels as we can't decode them
     labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
     # Decode reference summaries into text
     decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
     # ROUGE expects a newline after each sentence
     decoded_preds = ["\n".join(sent_tokenize(pred.strip())) for pred in decoded_preds]
-    decoded_labels = ["\n".join(sent_tokenize(label.strip())) for label in decoded_labels]
+    decoded_labels = [
+        "\n".join(sent_tokenize(label.strip())) for label in decoded_labels
+    ]
+
     # Compute ROUGE scores
     result = rouge_score.compute(
         predictions=decoded_preds, references=decoded_labels, use_stemmer=True
     )
-    # Extract the median scores
-    result = {key: value* 100 for key, value in result.items()}
+    # Extract the median scores and convert them to percentages
+    result = {key: value * 100 for key, value in result.items()}
     return {k: round(v, 4) for k, v in result.items()}
 
 
+# Define a data collator for language modeling tasks
+# Set mlm (Masked Language Modeling) to False, as we are using GPT-2 for causal language modeling, not masked language modeling
 data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
 
-# Train the model
+# Initialize the Trainer with the model, training arguments, and datasets
 trainer = Trainer(
     model=model,
     args=training_args,
@@ -177,10 +148,13 @@ trainer = Trainer(
     data_collator=data_collator,
     compute_metrics=compute_metrics,
 )
+
+# Train and evaluate the model
 trainer.train()
 trainer.evaluate()
 
-trainer.save_model('mymodel')
+# Save the trained model
+trainer.save_model("mymodel")
 
-# alternative saving method and folder
-model.save_pretrained('mymodel_alt')
+# Alternative saving method and folder
+model.save_pretrained("mymodel_alt")
--- GPT2-coh.py
+++ GPT2-coh.py
@@ -1,58 +1,77 @@
 import torch
 from torch.utils.data import DataLoader
 from datasets import load_dataset
-from transformers import AutoModelForCausalLM, GPT2LMHeadModel, GPT2Tokenizer, GPT2Config, AdamW
+from transformers import AutoModelForCausalLM, GPT2Tokenizer, GPT2Config, AdamW
 from rouge import Rouge
 import datasets
 
-#torch.cuda.empty_cache()
-
 # Define the device to use (GPU or CPU)
-#device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-device = torch.device('cuda')
+device = torch.device("cuda")
+
 # Load the Reddit dataset and split it into train and test sets
-#dataset = datasets.load_dataset('reddit')['train']
+dataset = datasets.load_dataset("reddit")["train"].train_test_split(test_size=0.1)
 
-dataset =  datasets.load_dataset('reddit')['train'].train_test_split(test_size=0.1)
+train_dataset = dataset["train"][:50]
+eval_dataset = dataset["test"][50:55]
 
-train_dataset = dataset['train'][:50]
-eval_dataset = dataset['test'][50:55]
-
-
-
-rem_list = ['author', 'body', 'id', 'normalizedBody', 'subreddit', 'subreddit_id']
-
+# Remove unnecessary columns from the dataset
+rem_list = ["author", "body", "id", "normalizedBody", "subreddit", "subreddit_id"]
 print("\n Removing extra columns.")
 [train_dataset.pop(key) for key in rem_list]
 [eval_dataset.pop(key) for key in rem_list]
 print("\nDONE!\n")
- 
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+
+# Initialize the GPT-2 tokenizer and set padding properties
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 tokenizer.pad_token = tokenizer.eos_token
 tokenizer.padding_side = "left"
 
+
+# Function to preprocess data
 def preprocess_data(example):
+    input_text = []
+    target_text = []
 
-    input_text=[]
-    target_text=[]
-
-    for i in range(len(example['content'])):
+    # Concatenate the content and summary text with the TL;DR separator
+    for i in range(len(example["content"])):
         content = str(example["content"][i])
         summary = str(example["summary"][i])
         if content.strip() and summary.strip():
             input_text.append(content + " TL;DR " + summary)
             target_text.append(summary)
 
-    input_ids = tokenizer(input_text, return_tensors="pt", padding="max_length", return_overflowing_tokens=False, truncation=True, max_length=512)["input_ids"]
+    # Tokenize input and target text
+    input_ids = tokenizer(
+        input_text,
+        return_tensors="pt",
+        padding="max_length",
+        return_overflowing_tokens=False,
+        truncation=True,
+        max_length=512,
+    )["input_ids"]
     with tokenizer.as_target_tokenizer():
-        target_ids = tokenizer(target_text, return_tensors="pt", padding="max_length", return_overflowing_tokens=False,  truncation=True, max_length=64)["input_ids"]
+        target_ids = tokenizer(
+            target_text,
+            return_tensors="pt",
+            padding="max_length",
+            return_overflowing_tokens=False,
+            truncation=True,
+            max_length=64,
+        )["input_ids"]
     return {"input_ids": input_ids.squeeze(), "target_ids": target_ids.squeeze()}
 
+
+# Convert dictionaries to Dataset objects
 train_dataset = datasets.Dataset.from_dict(train_dataset)
 eval_dataset = datasets.Dataset.from_dict(eval_dataset)
 
-train_preprocessed = train_dataset.map(preprocess_data, batched=True, batch_size=100, remove_columns=["content", "summary"])
-eval_preprocessed = eval_dataset.map(preprocess_data, batched=True, batch_size=100, remove_columns=["content", "summary"])
+# Preprocess train and eval datasets
+train_preprocessed = train_dataset.map(
+    preprocess_data, batched=True, batch_size=100, remove_columns=["content", "summary"]
+)
+eval_preprocessed = eval_dataset.map(
+    preprocess_data, batched=True, batch_size=100, remove_columns=["content", "summary"]
+)
 
 # Prepare the data loaders
 train_dataloader = DataLoader(train_preprocessed, batch_size=1, shuffle=True)
@@ -66,7 +85,7 @@ model.to(device)
 
 # Training loop
 epochs = 2
-coh_steps = 3
+coh_steps = 3  # Chain of Hindsight steps
 optimizer = AdamW(model.parameters(), lr=5e-5)
 
 for epoch in range(epochs):
@@ -74,17 +93,29 @@ for epoch in range(epochs):
         optimizer.zero_grad()
         input_ids = torch.stack(batch["input_ids"]).to(device)
         target_ids = torch.stack(batch["target_ids"]).to(device)
-        #input_ids = batch["input_ids"].to(device)
-        #target_ids = batch["target_ids"].to(device)
 
         # Generate a chain of summaries (coh_steps) as input
+        # Initialize the chain of hindsight (coh) input with the original input_ids
         coh_input_ids = input_ids
+        # Iterate through the number of steps defined by coh_steps
         for _ in range(coh_steps):
             with torch.no_grad():
-                coh_output_ids = model.generate(coh_input_ids, max_length=64, do_sample=True, pad_token_id=tokenizer.pad_token_id, eos_token_id=tokenizer.eos_token_id)
+                # Generate a new summary conditioned on the previous summaries
+                # This is the core idea of the Chain of Hindsight
+                # It generates a new summary based on the current coh_input_ids (previous summaries)
+                coh_output_ids = model.generate(
+                    coh_input_ids,
+                    max_length=64,
+                    do_sample=True,
+                    pad_token_id=tokenizer.pad_token_id,
+                    eos_token_id=tokenizer.eos_token_id,
+                )
+            # Concatenate the new summary to the existing chain of summaries
+            # This forms a sequence of summaries that the model will use as input for the next iteration
             coh_input_ids = torch.cat((coh_input_ids, coh_output_ids), dim=1)
 
         # Train with the final summary conditioned on the chain of summaries
+        # The model is trained using the chain of summaries as input (coh_input_ids) and the ground truth target_ids as labels
         outputs = model(coh_input_ids, labels=target_ids)
         loss = outputs.loss
         loss.backward()
@@ -102,11 +133,15 @@ num_eval_batches = 0
 for batch in eval_dataloader:
     input_ids = torch.stack(batch["input_ids"]).to(device)
     target_ids = torch.stack(batch["target_ids"]).to(device)
-    #input_ids = batch["input_ids"].to(device)
-    #target_ids = batch["target_ids"].to(device)
 
+    # Generate summaries for the evaluation dataset
     with torch.no_grad():
-        output_ids = model.generate(input_ids, max_length=64, pad_token_id=tokenizer.pad_token_id, eos_token_id=tokenizer.eos_token_id)
+        output_ids = model.generate(
+            input_ids,
+            max_length=64,
+            pad_token_id=tokenizer.pad_token_id,
+            eos_token_id=tokenizer.eos_token_id,
+        )
 
     # Decode the generated summaries and reference summaries
     generated_summaries = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
